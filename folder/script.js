7e6594ffe7f99539e05991e7174bb35bb0c84528
carlosgazabon <carlose.gazabon@hotmail.com>
2024-05-21 19:45:32 -0500

    commit

---
+++ folder/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var myButton = document.getElementById('myButton');
+    myButton.addEventListener('click', function() {
+        alert('¡Hola, Mundo!');
+    });
+});
